ea53511e48eaa57eac677e3682e9f49c225ce112
Grygorii Iermolenko <gyermolenko@gmail.com>
2017-11-16 16:55:18 +0200

    fix flake errors

--- demos/polls/aiohttpdemo_polls/db.py
+++ demos/polls/aiohttpdemo_polls/db.py
@@ -8,14 +8,17 @@ __all__ = ['question', 'choice']
 
 meta = MetaData()
 
+question = Table(
+    'question', meta,
 
-question = Table('question', meta,
     Column('id', Integer, primary_key=True),
     Column('question_text', String(200), nullable=False),
     Column('pub_date', Date, nullable=False)
 )
 
-choice = Table('choice', meta,
+choice = Table(
+    'choice', meta,
+
     Column('id', Integer, primary_key=True),
     Column('question_id', Integer, nullable=False),
     Column('choice_text', String(200), nullable=False),
