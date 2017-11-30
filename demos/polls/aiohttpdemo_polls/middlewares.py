7b7e333d9f0598c9da490df36d1ea4986621212d
Grygorii Iermolenko <gyermolenko@gmail.com>
2017-11-30 18:50:29 +0200

    Update middlewares file and docs with decorator from 2.3

--- demos/polls/aiohttpdemo_polls/middlewares.py
+++ demos/polls/aiohttpdemo_polls/middlewares.py
@@ -2,41 +2,41 @@ import aiohttp_jinja2
 from aiohttp import web
 
 
-async def handle_404(request, response):
-    response = aiohttp_jinja2.render_template('404.html',
-                                              request,
-                                              {})
-    return response
+async def handle_404(request):
+    return aiohttp_jinja2.render_template('404.html', request, {})
 
 
-async def handle_500(request, response):
-    response = aiohttp_jinja2.render_template('500.html',
-                                              request,
-                                              {})
-    return response
+async def handle_500(request):
+    return aiohttp_jinja2.render_template('500.html', request, {})
 
 
-def error_pages(overrides):
-    async def middleware(app, handler):
-        async def middleware_handler(request):
-            try:
-                response = await handler(request)
-                override = overrides.get(response.status)
-                if override is None:
-                    return response
-                else:
-                    return await override(request, response)
-            except web.HTTPException as ex:
-                override = overrides.get(ex.status)
-                if override is None:
-                    raise
-                else:
-                    return await override(request, ex)
-        return middleware_handler
-    return middleware
+def create_error_middleware(overrides):
+
+    @web.middleware
+    async def error_middleware(request, handler):
+
+        try:
+            response = await handler(request)
+
+            override = overrides.get(response.status)
+            if override:
+                return await override(request)
+
+            return response
+
+        except web.HTTPException as ex:
+            override = overrides.get(ex.status)
+            if override:
+                return await override(request)
+
+            raise
+
+    return error_middleware
 
 
 def setup_middlewares(app):
-    error_middleware = error_pages({404: handle_404,
-                                    500: handle_500})
+    error_middleware = create_error_middleware({
+        404: handle_404,
+        500: handle_500
+    })
     app.middlewares.append(error_middleware)
