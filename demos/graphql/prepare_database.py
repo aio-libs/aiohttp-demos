592d4b411c5015de450124ec6606950310d4882f
misha.gavela <Arfey17.mg@gmail.com>
2018-06-18 03:53:11 +0300

    fix: fixed flake8

--- demos/graphql/prepare_database.py
+++ demos/graphql/prepare_database.py
@@ -76,15 +76,14 @@ async def generate_users(conn: SAConnection, count: int) -> List[int]:
             'password': 'password',
             'avatar_url': (
                 'https://cdn.pixabay.com/photo/2016/08/08/09/17/'
-                'avatar-1577909_960_720.png')
-            ,
+                'avatar-1577909_960_720.png'
+            ),
             'gender': random.choice(list(UserGender)).value
         })
 
     response = await conn.execute(
         users.insert().values(values).returning(users.c.id)
     )
-
 
     return [user[0] for user in response]
 
