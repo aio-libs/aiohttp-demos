a2135772ddc19294daed2d2bb8f0285a3b9d067d
Sam Bull <aa6bs0@sambull.org>
2022-11-03 19:43:01 +0000

    Update handlers.py

--- demos/moderator_bot/moderator_bot/handlers.py
+++ demos/moderator_bot/moderator_bot/handlers.py
@@ -1,3 +1,4 @@
+import asyncio
 from functools import partial
 
 import numpy as np
@@ -7,13 +8,11 @@ from .worker import predict
 
 
 class MainHandler:
-    def __init__(self, loop, executor, slack_client, giphy_client):
-        self.loop = loop
+    def __init__(self, executor, slack_client, giphy_client):
         self.executor = executor
         self.slack_client = slack_client
         self.giphy_client = giphy_client
         self._toxicity_index = 0.4
-        self._run = partial(self.loop.run_in_executor, self.executor)
 
     async def listen_message(self, request):
         post = await request.json()
@@ -44,6 +43,7 @@ class MainHandler:
         )
 
     async def _message_handler(self, event):
-        scores = await asyncio.get_running_loop().run_in_executor(self.executor, predict, event["text"])
+        loop = asyncio.get_running_loop()
+        scores = await loop.run_in_executor(self.executor, predict, event["text"])
         if np.average([scores.toxic, scores.insult]) >= self._toxicity_index:
             await self._respond(event)
