b24fce02019741184d4ef297311eaf1754418ec5
cactuuus <jacopo.bob.calvi@gmail.com>
2023-02-12 14:37:55 +0000

    update to SQLAlchemy 2.0

--- demos/blog/db_helpers.py
+++ demos/blog/db_helpers.py
@@ -1,80 +1,77 @@
-from sqlalchemy import create_engine, MetaData
+import asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from aiohttpdemo_blog.db import construct_db_url
-from aiohttpdemo_blog.db import Users, Posts
+from aiohttpdemo_blog.db import Users, Posts, Base
 from aiohttpdemo_blog.security import generate_password_hash
 from aiohttpdemo_blog.settings import load_config
 
 
-def setup_db(executor_config=None, target_config=None):
+async def setup_db(executor_config=None, target_config=None):
     engine = get_engine(executor_config)
 
     db_name = target_config['DB_NAME']
     db_user = target_config['DB_USER']
     db_pass = target_config['DB_PASS']
 
-    with engine.connect() as conn:
-        conn.execute("CREATE USER %s WITH PASSWORD '%s'" % (db_user, db_pass))
-        conn.execute("CREATE DATABASE %s" % db_name)
-        conn.execute("ALTER DATABASE %s OWNER TO %s" % (db_name, db_user))
-        conn.execute("GRANT ALL ON SCHEMA public TO %s" % db_user)
+    async with engine.connect() as conn:
+        await conn.execute("CREATE USER %s WITH PASSWORD '%s'" % (db_user, db_pass))
+        await conn.execute("CREATE DATABASE %s" % db_name)
+        await conn.execute("ALTER DATABASE %s OWNER TO %s" % (db_name, db_user))
+        await conn.execute("GRANT ALL ON SCHEMA public TO %s" % db_user)
 
 
-def teardown_db(executor_config=None, target_config=None):
+async def teardown_db(executor_config=None, target_config=None):
     engine = get_engine(executor_config)
 
     db_name = target_config['DB_NAME']
     db_user = target_config['DB_USER']
 
-    with engine.connect() as conn:
+    async with engine.connect() as conn:
         # terminate all connections to be able to drop database
-        conn.execute("""
+        await conn.execute("""
           SELECT pg_terminate_backend(pg_stat_activity.pid)
           FROM pg_stat_activity
           WHERE pg_stat_activity.datname = '%s'
             AND pid <> pg_backend_pid();""" % db_name)
-        conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
-        conn.execute("REVOKE ALL ON SCHEMA public FROM %s" % db_user)
-        conn.execute("DROP ROLE IF EXISTS %s" % db_user)
+        await conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
+        await conn.execute("REVOKE ALL ON SCHEMA public FROM %s" % db_user)
+        await conn.execute("DROP ROLE IF EXISTS %s" % db_user)
 
 
 def get_engine(db_config):
     db_url = construct_db_url(db_config)
-    engine = create_engine(db_url, isolation_level='AUTOCOMMIT')
+    engine = create_async_engine(db_url, isolation_level='AUTOCOMMIT')
     return engine
 
 
-def create_tables(target_config=None):
+async def create_tables(target_config=None):
     engine = get_engine(target_config)
+    Session = async_sessionmaker(engine)
+    async with Session.begin() as sess:
+        await sess.run_sync(Base.metadata.create_all)
 
-    meta = MetaData()
-    meta.create_all(bind=engine, tables=[Users, Posts])
-
-
-def drop_tables(target_config=None):
+async def drop_tables(target_config=None):
     engine = get_engine(target_config)
+    Session = async_sessionmaker(engine)
+    async with Session.begin() as sess:
+        await sess.run_sync(Base.metadata.drop_all)
 
-    meta = MetaData()
-    meta.drop_all(bind=engine, tables=[Users, Posts])
 
-
-def create_sample_data(target_config=None):
+async def create_sample_data(target_config=None):
     engine = get_engine(target_config)
+    Session = async_sessionmaker(engine)
 
-    with engine.connect() as conn:
-        conn.execute(Users.insert(), [
-            {'username': 'Adam',
-             'email': 'adam@one.com',
-             'password_hash': generate_password_hash('adam')},
-            {'username': 'Bob',
-             'email': 'bob@two.com',
-             'password_hash': generate_password_hash('bob')},
-        ])
-        conn.execute(Posts.insert(), [
-            {'user_id': 1, 'body': 'Lovely day'},
-            {'user_id': 2, 'body': 'Roses are red'},
-            {'user_id': 2, 'body': 'Lorem ipsum'},
-        ])
+    async with Session.begin() as sess:
+        sess.add_all((
+            Users(username="Adam", email="adam@one.com",
+                  password_hash=generate_password_hash("adam")),
+            Users(username="Bob", email="bob@two.com",
+                  password_hash=generate_password_hash("bob")),
+            Posts(user_id=1, body="Lovely day"),
+            Posts(user_id=2, body="Roses are red"),
+            Posts(user_id=2, body="Lorem ipsum")
+        ))
 
 
 if __name__ == '__main__':
@@ -98,22 +95,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.create:
-        setup_db(executor_config=admin_db_config,
-                 target_config=user_db_config)
+        asyncio.run(setup_db(executor_config=admin_db_config,
+                 target_config=user_db_config))
     elif args.drop:
-        teardown_db(executor_config=admin_db_config,
-                    target_config=user_db_config)
+        asyncio.run(teardown_db(executor_config=admin_db_config,
+                    target_config=user_db_config))
     elif args.recreate:
-        teardown_db(executor_config=admin_db_config,
-                    target_config=user_db_config)
-        setup_db(executor_config=admin_db_config,
-                 target_config=user_db_config)
+        asyncio.run(teardown_db(executor_config=admin_db_config,
+                    target_config=user_db_config))
+        asyncio.run(setup_db(executor_config=admin_db_config,
+                 target_config=user_db_config))
     elif args.all:
-        teardown_db(executor_config=admin_db_config,
-                    target_config=user_db_config)
-        setup_db(executor_config=admin_db_config,
-                 target_config=user_db_config)
-        create_tables(target_config=user_db_config)
-        create_sample_data(target_config=user_db_config)
+        asyncio.run(teardown_db(executor_config=admin_db_config,
+                    target_config=user_db_config))
+        asyncio.run(setup_db(executor_config=admin_db_config,
+                 target_config=user_db_config))
+        asyncio.run(create_tables(target_config=user_db_config))
+        asyncio.run(create_sample_data(target_config=user_db_config))
     else:
         parser.print_help()
--- demos/polls/init_db.py
+++ demos/polls/init_db.py
@@ -1,37 +1,41 @@
 # polls/init_db.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import asyncio
 from datetime import date
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from aiohttpdemo_polls.db import Question, Choice, Base
 from aiohttpdemo_polls.settings import BASE_DIR, get_config
 
-DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
+DSN = "postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}"
 
 ADMIN_DB_URL = DSN.format(
     user='postgres', password='postgres', database='postgres',
     host='localhost', port=5432
 )
-admin_engine = create_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
+admin_engine = create_async_engine(ADMIN_DB_URL, isolation_level='AUTOCOMMIT')
 
 USER_CONFIG_PATH = BASE_DIR / 'config' / 'polls.yaml'
 USER_CONFIG = get_config(['-c', USER_CONFIG_PATH.as_posix()])
 USER_DB_URL = DSN.format(**USER_CONFIG['postgres'])
-user_engine = create_engine(USER_DB_URL)
+user_engine = create_async_engine(USER_DB_URL)
 
 TEST_CONFIG_PATH = BASE_DIR / 'config' / 'polls_test.yaml'
 TEST_CONFIG = get_config(['-c', TEST_CONFIG_PATH.as_posix()])
 TEST_DB_URL = DSN.format(**TEST_CONFIG['postgres'])
-test_engine = create_engine(TEST_DB_URL)
+test_engine = create_async_engine(TEST_DB_URL)
 
 
-def create_tables(engine=test_engine):
-    Base.metadata.create_all(bind=engine)
+async def create_tables(engine=test_engine):
+    Session = async_sessionmaker(engine)
+    async with Session.begin() as sess:
+        await sess.run_sync(Base.metadata.create_all)
+    
 
-def sample_data(engine=test_engine):
-    Session = sessionmaker(engine)
-    with Session.begin() as session:
-        session.add_all((
+async def sample_data(engine=test_engine):
+    Session = async_sessionmaker(engine)
+    async with Session.begin() as sess:
+        sess.add_all((
             Question(question_text="What\'s new?",pub_date=date(2015, 12, 15)),
             Choice(choice_text="Not much", votes=0, question_id=1),
             Choice(choice_text="The sky", votes=0, question_id=1),
@@ -39,5 +43,5 @@ def sample_data(engine=test_engine):
         ))
 
 if __name__ == "__main__":
-    create_tables(engine=user_engine)
-    sample_data(engine=user_engine)
+    asyncio.run(create_tables(engine=user_engine))
+    asyncio.run(sample_data(engine=user_engine))
