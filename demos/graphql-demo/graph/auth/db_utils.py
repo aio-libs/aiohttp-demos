4cbbf2432e50000016f3b269e3d14685b94c26d4
Sam Bull <git@sambull.org>
2023-09-19 16:58:22 +0100

    Update db_utils.py

--- demos/graphql-demo/graph/auth/db_utils.py
+++ demos/graphql-demo/graph/auth/db_utils.py
@@ -1,19 +1,15 @@
 from typing import List
 
-from graph.auth.tables import Users
+from graph.auth.models import User
 
 from sqlalchemy.sql import select
 
 
 async def select_users(session, keys: List[int]):
-    cursor = await session.scalars(select(Users)
-                                   .where(Users.id.in_(keys))
-                                   .order_by(Users.id))
+    cursor = await session.scalars(select(User).where(User.id.in_(keys)).order_by(User.id))
     return await cursor.all()
 
 
 async def select_user(session, key: int):
-    cursor = await session.scalars(select(Users)
-                                   .where(Users.id == key)
-                                   .order_by(Users.id))
+    cursor = await session.scalars(select(User).where(User.id == key).order_by(User.id))
     return await cursor.first()
