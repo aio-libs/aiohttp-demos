5f335ae539b5025f6247d66c806c563f7622caa7
Louis Maddox <louismmx@gmail.com>
2024-11-09 13:04:18 +0000

    fix: use db_sm without calling first

--- demos/graphql-demo/graph/auth/tests/test_auth_db_utils.py
+++ demos/graphql-demo/graph/auth/tests/test_auth_db_utils.py
@@ -6,9 +6,7 @@ from graph.auth.db_utils import select_user, select_users
 
 @pytest.mark.asyncio
 async def test_select_user(db_sm):
-    session = db_sm()
-
-    async with session.begin() as sess:
+    async with db_sm.begin() as sess:
         res = await select_user(sess, 1)
 
     assert res.id == 1
@@ -16,9 +14,7 @@ async def test_select_user(db_sm):
 
 @pytest.mark.asyncio
 async def test_select_users(db_sm):
-    session = db_sm()
-
-    async with session.begin() as sess:
+    async with db_sm.begin() as sess:
         res = await select_users(sess, [1, 2, 3])
 
     assert isinstance(res, list)
