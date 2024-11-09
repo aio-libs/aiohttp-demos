152a6b46f2b89a8bd88d326a6390a60cf2bb32fe
Louis Maddox <louismmx@gmail.com>
2024-11-09 18:13:02 +0000

    fix: result not a cursor

--- demos/graphql-demo/graph/auth/db_utils.py
+++ demos/graphql-demo/graph/auth/db_utils.py
@@ -6,10 +6,12 @@ from sqlalchemy.sql import select
 
 
 async def select_users(session, keys: List[int]):
-    cursor = await session.scalars(select(User).where(User.id.in_(keys)).order_by(User.id))
-    return await cursor.all()
+    result = await session.scalars(
+        select(User).where(User.id.in_(keys)).order_by(User.id)
+    )
+    return result.all()
 
 
 async def select_user(session, key: int):
-    cursor = await session.scalars(select(User).where(User.id == key).order_by(User.id))
-    return await cursor.first()
+    result = await session.scalars(select(User).where(User.id == key).order_by(User.id))
+    return result.first()
