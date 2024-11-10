8f929104b8e2a1fd688001cabd76a71b2652f8ba
Louis Maddox <louismmx@gmail.com>
2024-11-10 17:30:03 +0000

    fix: missing expire_on_commit param in async_sessionmaker call in base dataloader

--- demos/graphql-demo/graph/api/dataloaders.py
+++ demos/graphql-demo/graph/api/dataloaders.py
@@ -1,13 +1,13 @@
 from typing import Any, List
 
 from aiodataloader import DataLoader
-
-from sqlalchemy.ext.asyncio import async_sessionmaker
 from graph.auth.db_utils import select_users
 from graph.auth.models import User
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
-
-__all__ = ['UserDataLoader', ]
+__all__ = [
+    "UserDataLoader",
+]
 
 
 class BaseAIODataLoader(DataLoader):
@@ -15,18 +15,17 @@ class BaseAIODataLoader(DataLoader):
 
     It need create when application initialization with current db engine.
     """
+
     engine: Any = None
 
     def __init__(self, engine, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.engine = engine
-        self.session = async_sessionmaker(self.engine)
+        self.session = async_sessionmaker(self.engine, expire_on_commit=False)
 
     def sorted_by_keys(self, items: list[User], keys: List[int]) -> list[User]:
         """Help ordering of returned items In `aiodataloader`."""
-        items_dict = {
-            key: value for key, value in zip(sorted(set(keys)), items)
-        }
+        items_dict = {key: value for key, value in zip(sorted(set(keys)), items)}
 
         return [items_dict[key] for key in keys]
 
@@ -36,6 +35,7 @@ class UserDataLoader(BaseAIODataLoader):
 
     Should be used everywhere, when it is possible problem N + 1 requests.
     """
+
     async def batch_load_fn(self, keys: List[int]) -> list[User]:
         async with self.session.begin() as sess:
             response = await select_users(sess, keys)
