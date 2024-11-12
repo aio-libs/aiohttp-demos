6802d16186b095002814212d4a1772e938888d70
Sam Bull <git@sambull.org>
2024-11-12 13:33:27 +0000

    Fix

--- demos/blog/aiohttpdemo_blog/typedefs.py
+++ demos/blog/aiohttpdemo_blog/typedefs.py
@@ -3,5 +3,5 @@ from typing import Any
 from aiohttp import web
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-config = web.AppKey("config", dict[str, Any])
-db_pool = web.AppKey("db", async_sessionmaker)
+config_key = web.AppKey("config_key", dict[str, Any])
+db_key = web.AppKey("db_key", async_sessionmaker)
