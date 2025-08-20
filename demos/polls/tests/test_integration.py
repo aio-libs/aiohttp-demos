252b1b9fd3c4aeb70378f669712dc83feddfcc4b
Sam Bull <git@sambull.org>
2025-08-20 13:11:39 +0100

    Update test_integration.py

--- demos/polls/tests/test_integration.py
+++ demos/polls/tests/test_integration.py
@@ -5,22 +5,22 @@ from aiohttpdemo_polls.typedefs import db_key
 
 
 async def test_index(cli, tables_and_data):
-    response = await cli.get('/')
-    assert response.status == 200
-    # TODO: resolve question with html code "&#39;" instead of apostrophe in
-    # assert 'What\'s new?' in await response.text()
-    assert 'Main' in await response.text()
+    async with cli.get('/') as response:
+        assert response.status == 200
+        # TODO: resolve question with html code "&#39;" instead of apostrophe in
+        # assert 'What\'s new?' in await response.text()
+        assert 'Main' in await response.text()
 
 
 async def test_results(cli, tables_and_data):
-    response = await cli.get('/poll/1/results')
-    assert response.status == 200
-    assert 'Just hacking again' in await response.text()
+    async with cli.get('/poll/1/results') as response:
+        assert response.status == 200
+        assert 'Just hacking again' in await response.text()
 
 
 async def test_404_status(cli, tables_and_data):
-    response = await cli.get('/no-such-route')
-    assert response.status == 404
+    async with cli.get('/no-such-route') as response:
+        assert response.status == 404
 
 
 async def test_vote(cli, tables_and_data):
@@ -38,11 +38,11 @@ async def test_vote(cli, tables_and_data):
         not_much_choice_id = not_much_choice.id
         votes_before = not_much_choice.votes
 
-        response = await cli.post(
+        async with cli.post(
             f'/poll/{question_id}/vote',
             data={'choice': not_much_choice_id}
-        )
-        assert response.status == 200
+        ) as response:
+            assert response.status == 200
 
     async with cli.server.app[db_key].begin() as sess:
         result = await sess.scalars(
